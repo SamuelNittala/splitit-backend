83c2af6d9df15b60b945f35482f550bac3d2e8ff
samuelnittala <samuelnittala@pop-os.localdomain>
2023-05-29 14:03:01 +0530

    Add test profile and basic register test.

---
+++ src/test/java/com/splitit/main/UserRegistrationIntegrationTest.java
@@ -0,0 +1,41 @@
+package com.splitit.main;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.splitit.entity.User;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserRegistrationIntegrationTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
+
+	@Test
+	public void shouldCreateNewUser() throws Exception {
+		User user = new User();
+		user.setUserName("testUser");
+		user.setPassword("password123");
+		user.setEmail("testUser@example.com");
+		user.setFirstName("testFirst");
+		user.setLastName("testLast");
+
+		mockMvc.perform(post("/register").contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(user)))
+				.andExpect(status().isOk());
+	}
+}
