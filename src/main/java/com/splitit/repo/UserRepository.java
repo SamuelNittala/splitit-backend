65af2c4cc67df0ddfc1f6bde7a187ec3e04500a5
samuelnittala <samuelnittala@pop-os.localdomain>
2023-05-30 20:06:52 +0530

    UserRegister Changes
    1. Fix UserRegistrationIntegrationTest
    
    2. Add deleteById() for userRepository.

--- src/main/java/com/splitit/repo/UserRepository.java
+++ src/main/java/com/splitit/repo/UserRepository.java
@@ -2,11 +2,11 @@ package com.splitit.repo;
 
 import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.splitit.entity.User;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
 	Optional<User> findByUserName(String userName);
 
